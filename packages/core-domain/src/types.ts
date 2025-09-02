d583b853605c98b4db878d66de0baa292afce29f
Olaobaju Abraham <olaobajua@gmail.com>
2025-09-02 16:13:44 +0000

    update types.ts

--- packages/core-domain/src/types.ts
+++ packages/core-domain/src/types.ts
@@ -1,2 +1,13 @@
-type PaymentIntent = {}
-type SupportedCurrencies = ['NGN', 'USD','GBP']
+type PaymentIntent = {
+    _id: string;
+    amount: number;
+    currency: string;
+    payment: {
+        medium: string;
+        network: string;
+    },
+    trace: object,
+    created_at: Date,
+    updated_at: Date
+}
+type SupportedCurrencies = ['NGN', 'USD', 'GBP', 'PHP','']
